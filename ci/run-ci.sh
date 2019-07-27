4caca17c5c9f5a3d43c71712b124905f588e279d
Mickael Carl <contact+git@carlm.org>
2019-07-27 15:27:03 +0200

    Make sure cleanup is always run

--- ci/run-ci.sh
+++ ci/run-ci.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+worker="worker-ubuntu16-04"
+
 # From https://github.com/bazelbuild/bazel/blob/master/tools/bash/runfiles/runfiles.bash:
 set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
 source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
@@ -9,12 +11,16 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
   source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
 
+cleanup() {
+  rm -rf "${worker}"
+  docker-compose -f $(rlocation "com_github_buildbarn_bb_deployments/ci/docker-compose.yml") down
+}
+trap cleanup 0
+
 set -ux
 set +e
 
 export CURWD=$(pwd)
-
-worker="worker-ubuntu16-04"
 
 mkdir -pm 0777 "${worker}" "${worker}/build"
 mkdir -m 0700 "${worker}/cache"
@@ -22,6 +28,3 @@ mkdir -m 0700 "${worker}/cache"
 docker-compose -f $(rlocation "com_github_buildbarn_bb_deployments/ci/docker-compose.yml") up -d && \
 docker-compose -f $(rlocation "com_github_buildbarn_bb_deployments/ci/docker-compose-build.yml") run --rm builder && \
 docker-compose -f $(rlocation "com_github_buildbarn_bb_deployments/ci/docker-compose-build.yml") run --rm imager
-docker-compose -f $(rlocation "com_github_buildbarn_bb_deployments/ci/docker-compose.yml") down
-
-rm -rf "${worker}"
