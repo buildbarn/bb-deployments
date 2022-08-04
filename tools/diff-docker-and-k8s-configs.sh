b74b4abca29301a925eafb21741e58d276ffa90f
Fredrik Medley <fredrik@meroton.com>
2022-08-04 10:02:07 +0200

    Test the diff between docker-compose and k8s configs
    There should be limited differences between the Docker Compose and
    Kubernetes configurations for Buildbarn. Until there is an integration
    test for the Kubernetes deployment, check that the differences between
    the configurations is minimal. Every now and then, we should also
    remember to manually deploy and make sure it actually works on
    Kubernetes.

---
+++ tools/diff-docker-and-k8s-configs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eEu
+
+function diff_config() {
+    k8s_config="$1"
+    docker_config="$2"
+    # Allow diffs, i.e. exit code 1 from git-diff.
+    cat "$k8s_config" | grep -E '^(    |$)' | cut -c5- | \
+        (git diff --no-index - "$docker_config" || [ $? == 1 ])
+}
+
+diff_config kubernetes/config/browser.yaml docker-compose/config/browser.jsonnet
+diff_config kubernetes/config/common.yaml docker-compose/config/common.libsonnet
+diff_config kubernetes/config/frontend.yaml docker-compose/config/frontend.jsonnet
+diff_config kubernetes/config/runner-ubuntu22-04.yaml docker-compose/config/runner-ubuntu22-04.jsonnet
+diff_config kubernetes/config/scheduler.yaml docker-compose/config/scheduler.jsonnet
+diff_config kubernetes/config/storage.yaml docker-compose/config/storage.jsonnet
+diff_config kubernetes/config/worker-ubuntu22-04.yaml docker-compose/config/worker-ubuntu22-04.jsonnet
