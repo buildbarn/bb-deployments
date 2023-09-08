82cdb41a4dbecca8259990dccaa49441c1813c32
Nils Wireklint <nils@meroton.com>
2023-09-08 11:49:17 +0200

    Document the docker-image versions

--- tools/test-deployment-docker-compose.sh
+++ tools/test-deployment-docker-compose.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+
 # Verifies that the docker-compose deployment works.
-set -eEuxo pipefail
+set -eux -o pipefail
 
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 cd "${script_dir}/../docker-compose"
