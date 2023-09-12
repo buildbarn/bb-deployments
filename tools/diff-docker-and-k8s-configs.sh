564fc03fd023217f3d7ddd9a8acc9b9bac4a14f6
Fredrik Medley <fredrik@meroton.com>
2023-09-12 14:24:02 +0200

    Reenable the k8s vs docker config diff check
    This reverts commit 59d809ecf4f6522d39617b5965f21678ed5e8c3d,
    "Temporarily disable the k8s vs docker config diff check".
    
    The script was disabled while introducing FUSE in the docker-compose
    deployment.

--- tools/diff-docker-and-k8s-configs.sh
+++ tools/diff-docker-and-k8s-configs.sh
@@ -15,4 +15,4 @@ diff_config kubernetes/config/frontend.yaml docker-compose/config/frontend.jsonn
 diff_config kubernetes/config/runner-ubuntu22-04.yaml docker-compose/config/runner-ubuntu22-04.jsonnet
 diff_config kubernetes/config/scheduler.yaml docker-compose/config/scheduler.jsonnet
 diff_config kubernetes/config/storage.yaml docker-compose/config/storage.jsonnet
-diff_config kubernetes/config/worker-ubuntu22-04.yaml docker-compose/config/worker-ubuntu22-04.jsonnet
+diff_config kubernetes/config/worker-ubuntu22-04.yaml docker-compose/config/worker-hardlinking-ubuntu22-04.jsonnet
