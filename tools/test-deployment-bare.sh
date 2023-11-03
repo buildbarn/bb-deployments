8ef0924b61405f284770e76886b9c3c6db8e32ba
Nils Wireklint <nils@meroton.com>
2023-11-03 09:54:49 +0100

    Tidy up tests

--- tools/test-deployment-bare.sh
+++ tools/test-deployment-bare.sh
@@ -33,7 +33,9 @@ bazel --output_base="$abseil_output_base" clean
 bazel --output_base="$abseil_output_base" test --color=no --curses=no --config=remote-local --disk_cache= @abseil-hello//:hello_test
 # Make sure there are remote executions but no cache hits.
 # INFO: 39 processes: 9 internal, 30 remote.
-cat "${abseil_output_base}/command.log" | grep -E '^INFO: [0-9]+ processes: .*[0-9]+ remote[.,]' | grep -v 'remote cache hit'
+grep -E '^INFO: [0-9]+ processes: .*[0-9]+ remote[.,]' \
+    "$abseil_output_base/command.log" \
+    | grep -v 'remote cache hit'
 
 # --- Check that we get cache hit even after rebooting the server ---
 kill "$buildbarn_pid"
@@ -45,4 +47,6 @@ bazel --output_base="$abseil_output_base" clean
 bazel --output_base="$abseil_output_base" test --color=no --curses=no --config=remote-local --disk_cache= @abseil-hello//:hello_test
 # Make sure there are remote cache hits but no remote executions.
 # INFO: 39 processes: 30 remote cache hit, 9 internal.
-cat "${abseil_output_base}/command.log" | grep -E '^INFO: [0-9]+ processes: .*[0-9]+ remote cache hit[.,]' | grep -v 'remote[,.]'
+grep -E '^INFO: [0-9]+ processes: .*[0-9]+ remote cache hit[.,]' \
+    "$abseil_output_base/command.log" \
+    | grep -v 'remote[,.]'
