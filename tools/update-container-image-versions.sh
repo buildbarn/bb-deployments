ad3f3cf911fb1387351ad0561fdca1765e8f3812
Nils Wireklint <nils@meroton.com>
2023-11-15 10:25:30 +0100

    Tidy up
    It is better to avoid the ',' sed separator, as that is used in many
    other listings of the command line (where it is typically removed).
    For instance, to run a sed command taken from 'strace':
    
        [pid 732776] execve("/usr/bin/sed", ["sed", "-i",
        "s#\\(ghcr\\.io/buildbarn/bb-storage:\\)[0-9tz]*-[0-9a-f]*#\\120231111T202247Z-ece87ab#g",
        "README.md", "docker-compose/docker-compose.yml",
        "kubernetes/bb-namespace.yaml", "kubernetes/browser.yaml",
        "kubernetes/frontend.yaml", "kubernetes/kustomization.yaml",
        "kubernetes/scheduler.yaml", "kubernetes/storage.yaml",
        "kubernetes/worker-ubuntu22-04.yaml"], 0x55cbf0d29900 /* 56 vars */)
        = 0
    
    We want to remove the commas from the arg array listing.
    A pound symbol is easier to retain in the sed expression.

--- tools/update-container-image-versions.sh
+++ tools/update-container-image-versions.sh
@@ -18,37 +18,38 @@ set -eu -o pipefail
 #             differences: T, Z and truncated hash.        ^      ^        ^^^^^
 
 get_image_version() {
-    repo="$1"
+    repo="$1"; shift
     grep -E "github\.com/buildbarn/${repo}" go.mod \
-        | sed 's,.* v0.0.0-\([0-9]\{8\}\)\([0-9]\{6\}\)-\([0-9a-f]\{7\}\)[0-9a-f]\+,\1T\2Z-\3,'
+        | sed 's#.* v0.0.0-\([0-9]\{8\}\)\([0-9]\{6\}\)-\([0-9a-f]\{7\}\)[0-9a-f]\+#\1T\2Z-\3#'
 }
 
 get_full_git_commit_hash() {
-    repo="$1"
+    repo="$1"; shift
     # Reuse the same sed expression as in get_image_version.
     short_commit_hash=$(grep -E "github\.com/buildbarn/${repo}" go.mod \
-        | sed 's,.* v0.0.0-\([0-9]\{8\}\)\([0-9]\{6\}\)-\([0-9a-f]\+\),\3,')
+        | sed 's#.* v0.0.0-\([0-9]\{8\}\)\([0-9]\{6\}\)-\([0-9a-f]\+\)#\3#')
     # Let GitHub resolve the full commit hash.
     curl "https://github.com/buildbarn/$repo/commit/$short_commit_hash" \
         | grep '<meta property="og:url" content="' \
-        | sed 's,.*<meta property="og:url" content="/buildbarn/'"$repo"'/commit/\([0-9a-f]*\)" />.*,\1,'
+        | sed 's#.*<meta property="og:url" content="/buildbarn/'"$repo"'/commit/\([0-9a-f]*\)" />.*#\1#'
 }
 
 update_image_version() {
-    repo="$1"
-    image_name="$2"
+    repo="$1"; shift
+    image_name="$1"; shift
+
     image_version=$(get_image_version "$repo")
-    timestamp=$(echo "$image_version" | sed 's,\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)T\([0-9]\{2\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)Z.*,\1-\2-\3 \4:\5:\6,')
+    timestamp=$(echo "$image_version" | sed 's#\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)T\([0-9]\{2\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)Z.*#\1-\2-\3 \4:\5:\6#')
     commit_hash=$(get_full_git_commit_hash "$repo")
 
     # Replace image version.
-    sed -i "s,\(ghcr\.io/buildbarn/${image_name}:\)[0-9tz]*-[0-9a-f]*,\1${image_version},g" \
+    sed -i "s#\(ghcr\.io/buildbarn/${image_name}:\)[0-9tz]*-[0-9a-f]*#\1${image_version}#g" \
         README.md docker-compose/docker-compose.yml kubernetes/*.yaml
 
     # Replace timestamp and CI Build link.
     sed -i \
-        -e "s,^\(| ${image_name} .*| \)\([^|]* UTC |\),\1${timestamp} UTC |," \
-        -e "s,| [^|]*/buildbarn/${repo}/commit/[0-9a-f]*/checks,| [\`${commit_hash}\`](https://github.com/buildbarn/${repo}/commit/${commit_hash}/checks," \
+        -e "s#^\(| ${image_name} .*| \)\([^|]* UTC |\)#\1${timestamp} UTC |#" \
+        -e "s#| [^|]*/buildbarn/${repo}/commit/[0-9a-f]*/checks#| [\`${commit_hash}\`](https://github.com/buildbarn/${repo}/commit/${commit_hash}/checks#" \
         README.md
 }
 
