buildbarn/bb-deployments

commit 4acd1958514ec90c286eefc7d3f513dfa1ba32b4

Add virtual file mount (FUSE) for increased performance
For any RBE builds that are gated in perf & spend most time fetching
inputs, use virtual mount to increase performance. This commit adds the
FUSE mount for the docker-compose deployment.

Closes #81

docker-compose/run.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
 
-worker="worker-ubuntu22-04"
+worker_fuse="worker-fuse-ubuntu22-04"
+worker_hardlinking="worker-hardlinking-ubuntu22-04"
+fuse_dir_to_unmount="volumes/${worker_fuse}/build"
 
-sudo rm -rf volumes/bb "volumes/${worker}"
-mkdir -m 0700 -p volumes
-mkdir -m 0777 "volumes/${worker}" "volumes/${worker}/build"
-mkdir -m 0700 "volumes/${worker}/cache"
+set -x
+sudo fusermount -u "$fuse_dir_to_unmount" && sleep 1 || true
+sudo rm -rf bb "volumes/${worker_fuse}" "volumes/${worker_hardlinking}"
+mkdir -p volumes
+mkdir -m 0777 "volumes/${worker_fuse}" "volumes/${worker_fuse}"/{build,cas,cas/persistent_state}
+mkdir -m 0777 "volumes/${worker_hardlinking}" "volumes/${worker_hardlinking}"/{build,cas,cas/persistent_state}
+mkdir -m 0700 "volumes/${worker_fuse}/cache" "volumes/${worker_hardlinking}/cache"
 mkdir -m 0700 -p volumes/storage-{ac,cas}-{0,1}/persistent_state
+set +x
 
-exec docker-compose up "$@"
+cleanup() {
+    EXIT_STATUS=$?
+    set -x
+    sudo fusermount -u "$fuse_dir_to_unmount" || true
+    exit $EXIT_STATUS
+}
+
+# If no arguments have been given, automatically unmount worker FUSE mount.
+# This avoids annoying problems when trying to cleanup after a simple test run of Buildbarn.
+if [ $# -eq 0 ]; then
+    echo "Registering automatic unmount for $fuse_dir_to_unmount"
+    trap cleanup EXIT
+else
+    echo "When finished, manually unmount $fuse_dir_to_unmount"
+fi
+docker-compose up "$@"

.bazelrc
@@ -19,7 +19,7 @@ build:remote-exec --remote_executor=grpc://localhost:8980
 # The Buildbarn worker can configure instance_name_prefix to create separate
 # execution bins within the cluster. Optional, but useful when trying new
 # worker configurations.
-build:remote-exec --remote_instance_name=remote-execution
+build:remote-exec --remote_instance_name=fuse
 # Make sure to load Buildbarn with more requests than the number of CPUs on
 # your host machine.
 build:remote-exec --jobs=64

docker-compose/config/worker-fuse-ubuntu22-04.jsonnet
@@ -1,0 +1,101 @@
+local common = import 'common.libsonnet';
+
+// The FUSE worker is the most efficient configuration.
+// This is preferred to the hardlinking configuration.
+{
+  blobstore: {
+    actionCache: common.blobstore.actionCache,
+    contentAddressableStorage: {
+      readCaching: {
+        slow: common.blobstore.contentAddressableStorage,
+        fast: {
+          'local': {
+            keyLocationMapOnBlockDevice: {
+              file: {
+                path: '/worker/cas/key_location_map',
+                sizeBytes: 100 * 1024 * 1024,
+              },
+            },
+            keyLocationMapMaximumGetAttempts: 8,
+            keyLocationMapMaximumPutAttempts: 32,
+            oldBlocks: 8,
+            currentBlocks: 24,
+            newBlocks: 1,
+            blocksOnBlockDevice: {
+              source: {
+                file: {
+                  path: '/worker/cas/blocks',
+                  sizeBytes: 8 * 1024 * 1024 * 1024,
+                },
+              },
+              spareBlocks: 3,
+              dataIntegrityValidationCache: {
+                cacheSize: 50000,
+                cacheDuration: '14400s',
+                cacheReplacementPolicy: 'LEAST_RECENTLY_USED',
+              },
+            },
+            persistent: {
+              stateDirectoryPath: '/worker/cas/persistent_state',
+              minimumEpochInterval: '300s',
+            },
+          },
+        },
+        replicator: { deduplicating: { 'local': {} } },
+      },
+    },
+  },
+  browserUrl: common.browserUrl,
+  maximumMessageSizeBytes: common.maximumMessageSizeBytes,
+  scheduler: { address: 'scheduler:8983' },
+  global: common.global,
+  buildDirectories: [{
+    virtual: {
+      maximumExecutionTimeoutCompensation: '3600s',
+      shuffleDirectoryListings: true,
+      mount: {
+        mountPath: '/worker/build',
+        fuse: {
+          directoryEntryValidity: '300s',
+          inodeAttributeValidity: '300s',
+          allowOther: true,
+          directMount: true,
+        },
+      },
+    },
+    runners: [{
+      endpoint: { address: 'unix:///worker/runner' },
+      concurrency: 8,
+      instanceNamePrefix: 'fuse',
+      platform: {
+        properties: [
+          { name: 'OSFamily', value: 'Linux' },
+          { name: 'container-image', value: 'docker://ghcr.io/catthehacker/ubuntu:act-22.04@sha256:5f9c35c25db1d51a8ddaae5c0ba8d3c163c5e9a4a6cc97acd409ac7eae239448' },
+        ],
+      },
+      maximumFilePoolFileCount: 10000,
+      maximumFilePoolSizeBytes: 1 * 1024 * 1024 * 1024,
+      workerId: {
+        datacenter: 'amsterdam',
+        rack: '3',
+        slot: '10',
+        hostname: 'ubuntu-worker.example.com',
+      },
+    }],
+  }],
+  filePool: {
+    blockDevice: {
+      file: {
+        path: '/worker/filepool',
+        // concurrency * maximumFilePoolSizeBytes
+        sizeBytes: 8 * 1024 * 1024 * 1024,
+      },
+    },
+  },
+  outputUploadConcurrency: 11,
+  directoryCache: {
+    maximumCount: 1000,
+    maximumSizeBytes: 1000 * 1024,
+    cacheReplacementPolicy: 'LEAST_RECENTLY_USED',
+  },
+}

docker-compose/config/worker-hardlinking-ubuntu22-04.jsonnet
@@ -1,5 +1,8 @@
 local common = import 'common.libsonnet';
 
+// DO NOT USE the hardlinking configuration below unless really needed.
+// This example only exists for reference in situations
+// where the more efficient FUSE worker is not supported.
 {
   blobstore: common.blobstore,
   browserUrl: common.browserUrl,
@@ -17,6 +20,7 @@ local common = import 'common.libsonnet';
     runners: [{
       endpoint: { address: 'unix:///worker/runner' },
       concurrency: 8,
+      instanceNamePrefix: 'hardlinking',
       platform: {
         properties: [
           { name: 'OSFamily', value: 'Linux' },
@@ -24,7 +28,7 @@ local common = import 'common.libsonnet';
         ],
       },
       workerId: {
-        datacenter: 'paris',
+        datacenter: 'linkoping',
         rack: '4',
         slot: '15',
         hostname: 'ubuntu-worker.example.com',

docker-compose/docker-compose.yml
@@ -59,17 +59,28 @@ services:
     volumes:
     - ./config:/config
 
-  worker-ubuntu22-04:
+  runner-installer:
+    image: buildbarn/bb-runner-installer:20230213T172655Z-e0ae60c
+    volumes:
+    - ./volumes/bb:/bb
+
+  # The FUSE worker is the most efficient configuration.
+  worker-fuse-ubuntu22-04:
     image: buildbarn/bb-worker:20230213T172655Z-e0ae60c
     command:
-    - /config/worker-ubuntu22-04.jsonnet
-    ports:
-    - 7986:7986
+    - /config/worker-fuse-ubuntu22-04.jsonnet
+    # Need to be privileged for the FUSE mounting to work.
+    privileged: true
     volumes:
     - ./config:/config
-    - ./volumes/worker-ubuntu22-04:/worker
+    - type: bind
+      source: ./volumes/worker-fuse-ubuntu22-04
+      target: /worker
+      bind:
+        # Bidirectional mount to expose the FUSE mount.
+        propagation: shared
 
-  runner-ubuntu22-04:
+  runner-fuse-ubuntu22-04:
     image: ghcr.io/catthehacker/ubuntu:act-22.04@sha256:5f9c35c25db1d51a8ddaae5c0ba8d3c163c5e9a4a6cc97acd409ac7eae239448
     command:
     - sh
@@ -78,12 +89,38 @@ services:
     network_mode: none
     volumes:
     - ./config:/config
-    - ./volumes/worker-ubuntu22-04:/worker
     - ./volumes/bb:/bb
+    - type: bind
+      source: ./volumes/worker-fuse-ubuntu22-04
+      target: /worker
+      bind:
+        # HostToContainer mount to use the FUSE mount.
+        propagation: slave
     depends_on:
     - runner-installer
 
-  runner-installer:
-    image: buildbarn/bb-runner-installer:20230213T172655Z-e0ae60c
+  # For situations where the more efficient FUSE worker is not supported,
+  # the classic hardlinking example is shown here.
+  worker-hardlinking-ubuntu22-04:
+    image: buildbarn/bb-worker:20230213T172655Z-e0ae60c
+    command:
+    - /config/worker-hardlinking-ubuntu22-04.jsonnet
+    # Need to be privileged for the FUSE mounting to work.
+    privileged: true
     volumes:
+    - ./config:/config
+    - ./volumes/worker-hardlinking-ubuntu22-04:/worker
+
+  runner-ubuntu22-04-hardlinking:
+    image: ghcr.io/catthehacker/ubuntu:act-22.04@sha256:5f9c35c25db1d51a8ddaae5c0ba8d3c163c5e9a4a6cc97acd409ac7eae239448
+    command:
+    - sh
+    - -c
+    - while ! test -f /bb/installed; do sleep 1; done; exec /bb/tini -v -g -- /bb/bb_runner /config/runner-ubuntu22-04.jsonnet
+    network_mode: none
+    volumes:
+    - ./config:/config
     - ./volumes/bb:/bb
+    - ./volumes/worker-hardlinking-ubuntu22-04:/worker
+    depends_on:
+    - runner-installer

tools/test-deployment-docker-compose.sh
@@ -26,8 +26,6 @@ bazel test --color=no --curses=no --config=remote-ubuntu-22-04 --disk_cache= @ab
 cat "$bazel_command_log" | grep -E '^INFO: [0-9]+ processes: .*[0-9]+ remote[.,]' | grep -v 'remote cache hit'
 
 # --- Check that we get cache hit even after rebooting the server ---
-# Make sure the persistent state is written before shutdown.
-# sleep 2
 docker-compose down
 docker-compose up -d --force-recreate
 
@@ -36,3 +34,10 @@ bazel test --color=no --curses=no --config=remote-ubuntu-22-04 --disk_cache= @ab
 # Make sure there are remote cache hits but no remote executions.
 # INFO: 39 processes: 30 remote cache hit, 9 internal.
 cat "$bazel_command_log" | grep -E '^INFO: [0-9]+ processes: .*[0-9]+ remote cache hit[.,]' | grep -v 'remote[,.]'
+
+# --- Check that the hardlinking workers are available ---
+bazel clean
+bazel test --color=no --curses=no --config=remote-ubuntu-22-04 --remote_instance_name=hardlinking --disk_cache= @abseil-hello//:hello_test
+# Make sure there are remote executions but no cache hits.
+# INFO: 39 processes: 9 internal, 30 remote.
+cat "$bazel_command_log" | grep -E '^INFO: [0-9]+ processes: .*[0-9]+ remote[.,]' | grep -v 'remote cache hit'

commit c7175bf00298800d3864d909d14e5629b2f319aa

Fix wrong path in 'rm -rf volumes/bb'
Commit 4acd1958514ec90c286eefc7d3f513dfa1ba32b4 accidentally changed
"volumes/bb" to "bb". Fix that.

Relates to #28.

docker-compose/run.sh
@@ -11,7 +11,7 @@ setup () {
     set -x
 
     { sudo fusermount -u "$fuse_dir_to_unmount" && sleep 1; } || true
-    sudo rm -rf bb "volumes/${worker_fuse}" "volumes/${worker_hardlinking}"
+    sudo rm -rf volumes/bb "volumes/${worker_fuse}" "volumes/${worker_hardlinking}"
 
     mkdir -p volumes
     mkdir -m 0777 "volumes/${worker_fuse}" "volumes/${worker_fuse}"/{build,cas,cas/persistent_state}