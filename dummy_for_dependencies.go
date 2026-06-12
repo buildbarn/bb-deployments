f3bbdcf11f5001d171b8216c3ef88833bcf65d06
Oscar Falk <oscar@meroton.com>
2026-06-12 12:22:11 +0200

    Remove unnecessary deps in dummy_for_dependencies.go (#195)
    These dependencies were needed before Bzlmod was introduced.
    Also update instructions on how to update go.mod.

--- dummy_for_dependencies.go
+++ dummy_for_dependencies.go
@@ -1,20 +1,13 @@
-// Package dummyforgomod only exists to make 'go mod tidy' and Gazelle
+// Package dummyforgomod only exists to make 'bazel run @rules_go//go -- mod tidy' and Gazelle
 // automatically generate all the dependencies needed for the BUILD.bazel
 // files into go_dependencies.bzl.
 //
-// If this file contains code using the dependencies, `go mod tidy` will only
+// If this file contains code using the dependencies, `bazel run @rules_go//go -- mod tidy` will only
 // add the exact transitive dependencies that are needed for building. By
-// leaving this file without any code, `go mod tidy` adds all transitive
+// leaving this file without any code, `bazel run @rules_go//go -- mod tidy` adds all transitive
 // dependencies for the needed modules.
 package dummyforgomod
 
 import (
-	_ "github.com/bazelbuild/buildtools/buildifier"               // GitHub Workflow
-	_ "github.com/buildbarn/bb-browser/cmd/bb_browser"            // bb-browser
-	_ "github.com/buildbarn/bb-remote-execution/cmd/bb_runner"    // bb-runner
-	_ "github.com/buildbarn/bb-remote-execution/cmd/bb_scheduler" // bb-scheduler
-	_ "github.com/buildbarn/bb-remote-execution/cmd/bb_worker"    // bb-worker
-	_ "github.com/buildbarn/bb-storage/cmd/bb_storage"            // bb-storage
-	_ "golang.org/x/lint"                                         // GitHub Workflow
-	_ "mvdan.cc/gofumpt"                                          // GitHub Workflow
+	_ "golang.org/x/lint" // GitHub Workflow
 )
