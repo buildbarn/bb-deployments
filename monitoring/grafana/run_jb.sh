8cbadc0631eba7606a5b99924104f10487066e0e
Fredrik Medley <moroten@users.noreply.github.com>
2026-08-11 12:29:44 +0200

    Apply suggestions from code review
    Co-authored-by: Fredrik Medley <moroten@users.noreply.github.com>

--- monitoring/grafana/run_jb.sh
+++ monitoring/grafana/run_jb.sh
@@ -1,10 +1,13 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
 jb=$1
 jsonnetfile=$2
 jsonnetfile_lock=$3
 out_dir=$4
 
-cp $jsonnetfile .
-cp $jsonnetfile_lock .
+cp "$jsonnetfile" .
+cp "$jsonnetfile_lock" .
 
-$jb install
-cp -r --dereference vendor/* $out_dir
+"$jb" install
+cp -r --dereference vendor/* "$out_dir"
--- monitoring/grafana/run_jsonnet.sh
+++ monitoring/grafana/run_jsonnet.sh
@@ -6,5 +6,5 @@ jsonnet_imports=$2
 out_dir=$3
 dashboards_file=$4
 
-cp --dereference $dashboards_file dashboards.libsonnet
-$jsonnet $jsonnet_imports -m $out_dir dashboards.libsonnet
+cp --dereference "$dashboards_file" dashboards.libsonnet
+"$jsonnet" "$jsonnet_imports" -m "$out_dir" dashboards.libsonnet
